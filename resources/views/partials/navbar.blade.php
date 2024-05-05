d3588fa60727d3ad038f4a08c9dbe6c60dd5876d
arianisepti <arianidsepti01@gmail.com>
2024-05-05 22:30:34 +0700

    all changes

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,51 @@
+<div class="container-fluid nav-bar bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-primary navbar-light py-0 px-4">
+      <a
+        href="index.html"
+        class="navbar-brand d-flex align-items-center text-center"
+      >
+        <div class="icon p-2 me-2">
+          <img
+            class="img-fluid"
+            src="img2/Font Dan Symbol/Untitled-2.png"
+            alt="Icon"
+            style="width: 30px; height: 30px"
+          />
+        </div>
+        <h1 class="m-0 text-danger">Foursen</h1>
+      </a>
+      <button
+        type="button"
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto">
+          <a href="index.html" class="nav-item nav-link active">Home</a>
+          <a href="about.html" class="nav-item nav-link">About</a>
+          <a href="login.php" class="nav-item nav-link">Login</a>
+          
+          <div class="nav-item dropdown">
+            <a
+              href="#"
+              class="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+              >Pages</a
+            >
+            <div class="dropdown-menu rounded-0 m-0">
+              <a href="testimonial.html" class="dropdown-item"
+                >Testimonial</a
+              >
+            </div>
+          </div>
+          <a href="contact.html" class="nav-item nav-link">Contact</a>
+        </div>
+        <a href="" class="btn btn-primary px-3 d-none d-lg-flex"
+          >Add Property</a
+        >
+      </div>
+    </nav>
+  </div>
